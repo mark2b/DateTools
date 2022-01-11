d62fdc5def3bfa6807ca1896a4b1530f12de00c3
Mark Berner <mark2b@gmail.com>
2022-01-11 20:44:48 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "DateToolsSwift",
     targets: [
-        Target(name: "DateToolsSwift")
+        Target(name: "DateToolsSwift", path: "DateToolsSwift")
     ]
 )
 package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
